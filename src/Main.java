4bc4619961a083493c65706f2fa10507339bd405
Aakriti <aakrititiwari002@gmail.com>
2025-01-05 00:25:36 +0530

    uc11 commit

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,14 @@ import java.util.Random;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+        IEmployeeWage empWageBuilder = new EmpWageBuilder();
+
+        // Adding multiple companies to the builder
         empWageBuilder.addCompanyEmpWage("Company A", 20, 20, 100);
         empWageBuilder.addCompanyEmpWage("Company B", 25, 22, 120);
         empWageBuilder.addCompanyEmpWage("Company C", 30, 18, 90);
+
+        // Compute wages for all companies
         empWageBuilder.computeEmployeeWage();
     }
 }
