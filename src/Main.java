e56b28d99c45df858f5fd16f06e8293657eedbf4
Aakriti <aakrititiwari002@gmail.com>
2025-01-05 00:01:35 +0530

    uc1 commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        Employee e1 = new Employee("Ram", 123);
+        Random r = new Random();
+
+
+        int i = r.nextInt(2);
+        if (i == 1) {
+            e1.attendance(true);
+        } else {
+            e1.attendance(false);
+        }
+    }
+}
